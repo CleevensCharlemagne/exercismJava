8281a822f6b5c370729698757670dfb60e2638ee
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-31 19:37:55 -0500

    update

--- src/cookYourLasagna/Lasagna.java
+++ src/cookYourLasagna/Lasagna.java
@@ -1,4 +1,15 @@
 package cookYourLasagna;
 
 public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven(){
+        return 40;
+    }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
 }
+

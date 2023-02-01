fad190a07476e7e5f92f6ab94a1dfac1f2375033
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-31 20:34:57 -0500

    update

--- src/annalynsInfiltration/AnnalynsInfiltration.java
+++ src/annalynsInfiltration/AnnalynsInfiltration.java
@@ -14,7 +14,7 @@ class AnnalynsInfiltration {
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+        return (!knightIsAwake && !archerIsAwake && prisonerIsAwake && petDogIsPresent);
     }
 }
 

b02edcf1ff490631c396f50ebb6443c1a7b64e62
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-31 20:31:10 -0500

    update

--- src/annalynsInfiltration/AnnalynsInfiltration.java
+++ src/annalynsInfiltration/AnnalynsInfiltration.java
@@ -10,7 +10,7 @@ class AnnalynsInfiltration {
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        return !archerIsAwake && prisonerIsAwake;
+        return (!archerIsAwake && prisonerIsAwake);
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {

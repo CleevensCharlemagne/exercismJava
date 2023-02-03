24f3a5e93a28c94e660763050858c2f8b8ed4c2d
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-02 21:30:52 -0500

    update

--- src/birdWatcher/BirdWatcher.java
+++ src/birdWatcher/BirdWatcher.java
@@ -47,7 +47,7 @@ class BirdWatcher {
         int acc = 0;
 
         for (int i = 0; i < this.birdsPerDay.length; i++){
-            if (this.birdsPerDay[i] > 5){
+            if (this.birdsPerDay[i] >= 5){
                 acc++;
             }
         }
